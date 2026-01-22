0f8ed7643446e5716e63b9730047d58e73e64190
Pixelplanet <tom.winnerl@web.de>
2026-01-22 15:19:44 +0100

    Set isCompoundPath=true for valid multi-part shape rendering

--- src/lib/xcs-generator.js
+++ src/lib/xcs-generator.js
@@ -135,6 +135,7 @@ export class XCSGenerator {
     createPathDisplayWithPath(id, name, colorHex, colorInt, x, y, width, height, zOrder, dPath) {
         const display = this.createPathDisplay(id, name, colorHex, colorInt, x, y, width, height, zOrder);
         display.dPath = dPath;
+        display.isCompoundPath = true; // Vector output is almost always a compound path
         return display;
     }
 
